5c58b8bba7e3781b6a3ed6038558ec4f19888702
INJUN <ijhwang@pood.pet>
2023-02-26 13:05:00 +0900

    first commit

---
+++ lib/domain/repository/note_repository.dart
@@ -0,0 +1,13 @@
+import '../model/note.dart';
+
+abstract class NoteRepository {
+  Future<List<Note>> getNotes();
+
+  Future<Note?> getNoteById(int id);
+
+  Future<void> insertNote(Note note);
+
+  Future<void> updateNote(Note note);
+
+  Future<int> deleteNote(Note note);
+}
