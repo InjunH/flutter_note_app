c39d64a5905d75d4c181cc45dc9b26e3bc5131ab
INJUN <ijhwang@pood.pet>
2023-02-26 17:44:36 +0900

    DI 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_note_app/di/provider_setup.dart';
 import 'package:flutter_note_app/presentation/notes/note_screen.dart';
 import 'package:flutter_note_app/ui/colors.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  // flatform channel의 위젯 바인딩을 보장한다.
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final providers = await getProviders();
+
+  runApp(MultiProvider(
+    providers: providers,
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
